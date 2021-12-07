436b48aa78cfe553c67dc1195822bdafd3dd1f91
Silvia Quintero <silvia.quint.m@gmail.com>
2021-12-07 20:35:54 +0100

    change tags to  decorative lines

--- src/components/DecoVLines.tsx
+++ src/components/DecoVLines.tsx
@@ -4,7 +4,7 @@ import styles from './DecoVLines.module.css';
 const DecoVLines = () => {
     return (
         <div className={styles.decoLinesContainer}>
-            <span className={styles.decoLine1}></span>
+            <div className={styles.decoLine1}></div>
             <div className={styles.decoLine2}></div>
             <div className={styles.decoLine3}></div>
             <div className={styles.decoLine4}></div>
