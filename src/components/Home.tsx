7af017145113f1f1e6380e864b1954002c8f6862
Silvia Quintero <silvia.quint.m@gmail.com>
2021-12-09 13:07:13 +0100

    translate text in Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,10 +7,10 @@ const Home = () => {
 return (
     <div className={styles.homeContainer}>
         <div className={styles.welcomeContainer}>
-            <h1 className={styles.welcome}>Welcome to iHood Web-app</h1>
-            <p className={styles.textFormat}>The web-app where you can create your own neighborhood community, manage it and connect all its members in one place</p>
+            <h1 className={styles.welcome}>Bienvenidos a la web de iHood!</h1>
+            <p className={styles.textFormat}>Un lugar donde podrás crear tu propia comunidad de vecino, gestionarla y facilitar la comunicación entre todos sus integrantes.</p>
             <Icon className= {styles.icon} icon="noto:house-with-garden" />
-            <p className={styles.textFormat}>Come in, and discover a new way to neighbor communication</p>
+            <p className={styles.textFormat}>Entra y descubre una nueva forma de comunicarte con tus vecinos!</p>
         </div>
         <DecoVLines />
     </div>
