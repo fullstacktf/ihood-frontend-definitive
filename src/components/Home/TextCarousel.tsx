ded12ec16c0e521c454f24a8a5a3bbc1dbd36fde
Silvia Quintero <silvia.quint.m@gmail.com>
2021-12-14 12:44:20 +0100

    change the css import

--- src/components/Home/TextCarousel.tsx
+++ src/components/Home/TextCarousel.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import styles from './TextCarousel.module.css';
+import styles from './css/TextCarousel.module.css';
 
 class TextCarousel extends Component {
     render() {
